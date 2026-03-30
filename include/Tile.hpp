fdac1cbfd4060d611839ef05320299561fb2f83e
Lluís Aliaño Llufriu <lluisalillu@icloud.com>
2026-03-30 13:31:45 +0200

    feat: added tile class and its output and changed project name

---
+++ include/Tile.hpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <ostream>
+
+class Tile {
+ private:
+  enum Number {
+    missing_tile,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    thirteen,
+    fourteen,
+    fifteen,
+  };
+
+  Number m_value{};
+
+ public:
+  Tile() = default;
+  Tile(std::int32_t value) : m_value(static_cast<Number>(value)) {}
+
+  bool isEmpty() const;
+  std::int32_t getNum() const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Tile& tile);
---
+++ src/Tile.cpp
@@ -0,0 +1,18 @@
+#include <Tile.hpp>
+
+bool Tile::isEmpty() const {
+  return (m_value == Number::missing_tile) ? true : false;
+}
+
+std::int32_t Tile::getNum() const { return static_cast<std::int32_t>(m_value); }
+
+std::ostream& operator<<(std::ostream& out, const Tile& tile) {
+  std::string tileString{std::to_string(tile.getNum())};
+  if (tileString.size() == 1) {
+    tileString = ' ' + tileString;
+  }
+
+  out << ' ' << tileString << ' ';
+
+  return out;
+}
