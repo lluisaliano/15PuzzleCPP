3ee1de92be98abcd1cb5996b8b3b13e8f6e67251
Lluís Aliaño Llufriu <lluisalillu@icloud.com>
2026-03-31 21:07:40 +0200

    feat: Print empty tile in red

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -8,6 +8,10 @@ std::int32_t Tile::getNum() const { return static_cast<std::int32_t>(m_value); }
 
 std::ostream& operator<<(std::ostream& out, const Tile& tile) {
   std::string tileString{std::to_string(tile.getNum())};
+  // If Tile is the empty tile, print it in red
+  if (tile == Tile(0)) {
+    tileString = "\033[31m " + tileString + "\033[0m";
+  }
   if (tileString.size() == 1) {
     tileString = ' ' + tileString;
   }
